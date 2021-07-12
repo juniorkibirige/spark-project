2c335584d166a9257484b041a9590df4bc9e1be7
Junior Lawrence Kibirige <aanga26@gmail.com>
2021-07-12 23:42:53 +0300

    Main App with Auth flow

--- src/App.js
+++ src/App.js
@@ -1,39 +1,14 @@
 import './App.css';
-import { Switch, Route } from 'react-router-dom'
-import Login from './Pages/Login/Login.jsx';
-import Home from './Pages/Home/Home.jsx'
-import { useEffect, useState } from 'react';
-import Preferences from './Pages/Preferences/Preferences.jsx';
+import { Route, BrowserRouter } from 'react-router-dom'
+import Main from './Router'
 
 function App() {
-  const [auth, setAuth] = useState({token: ''})
-
-  const setToken = (userToken) => {
-    sessionStorage.setItem('token', userToken.token)
-  }
-
-  const getToken = () => {
-    const tokenString = sessionStorage.getItem('token')
-    setAuth({
-      token: tokenString
-    })
-  }
-
-  useEffect(() => {
-    getToken();
-    return ()=> setAuth({token: ''})
-  }, [])
 
   return (
     <>
-      <div>
-        <Switch>
-          <Route exact path='/' render={()=> auth.token.length === 0 ? <Login setToken={setToken}/> : <Home />} />
-          <Route path="/preferences">
-            <Preferences />
-          </Route>
-        </Switch>
-      </div>
+      <BrowserRouter>
+        <Route component={Main} />
+      </BrowserRouter>
     </>
   );
 }
