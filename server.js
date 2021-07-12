47ca4719524ded0a5f3d7f1efc2ca72c3066eb33
Junior Lawrence Kibirige <aanga26@gmail.com>
2021-07-12 23:38:31 +0300

    Testing offline server

--- server.js
+++ server.js
@@ -6,7 +6,12 @@ app.use(cors());
 
 app.use('/login', (req, res) => {
     res.send({
-        token: 'test123'
+        success: true,
+        id: "SP-USR-95267-UG",
+        name: "Admin Katumba",
+        email: "katumba@admin.com",
+        userType: 'admin',
+        token: "dNVH_-JxRzykyNsCeVSUm_:APA91bFj1LUMwfN6ZFpyLSUzO02hyoRzozbY-S5hfhJ_-3B1K0Y2pGdMXqipkGxuTkrwfLNMUC79gMGs_6FAerRSd_tYxrJ72jWLLR9iNtSazptN_Qmh7XaEcy9c7fvN2M6oh9u_Z4cS"
     })
 })
 
