eedf67512a4c5df8566d002c609251e9427ffc3c
Junior Lawrence Kibirige <aanga26@gmail.com>
2021-08-31 14:20:56 +0300

    custom-service-worker

--- workbox-config.js
+++ workbox-config.js
@@ -3,6 +3,6 @@ module.exports = {
 	globPatterns: [
 		'**/*.{json,png,js,ico,html,txt,css}'
 	],
-	swDest: 'src/service-worker-custom.js',
+	swDest: 'build/service-worker-custom.js',
 	swSrc: 'src/service-worker.js'
 };
